1ac4a719da284899af0ceed9cd4d35cb6b5a1158
jonatanare <jonatanare.dev@gmail.com>
2026-05-18 18:31:00 -0500

    update: Se agrego script de GTM y GA4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata, Viewport } from "next";
 import { Geist } from "next/font/google";
 import "./globals.css";
 import { QueryProvider } from "@/providers/QueryProvider";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,10 +31,51 @@ export const viewport: Viewport = {
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
+  const isProduction = process.env.NODE_ENV === "production";
   return (
     <html lang="es" className={`${geistSans.variable} h-full antialiased`}>
+      <head>
+        {isProduction && (
+          <>
+            <Script id="gtm-head" strategy="beforeInteractive">
+              {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-ND5DRSHZ');`}
+            </Script>
+
+          </>
+        )}
+      </head>
       <body className="min-h-full bg-white text-slate-900">
+        {isProduction && (
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-ND5DRSHZ"
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            />
+          </noscript>
+        )}
         <QueryProvider>{children}</QueryProvider>
+        {isProduction && (
+          <>
+            <Script
+              src="https://www.googletagmanager.com/gtag/js?id=G-LQY1VB1JKL"
+              strategy="afterInteractive"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+              {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-LQY1VB1JKL');
+          `}
+            </Script>
+          </>
+        )}
       </body>
     </html>
   );
